a7c535ed700711bbed22ef7940b9d6d0e0bbb604
tkachik88 <82943852+tkachik88@users.noreply.github.com>
2023-09-11 18:10:41 +0000

    Deploying to gh-pages from @ tkachik88/goit-js-hw-09@2f2f0425ea698edd17d0a6a89684eefa5ad1a839 🚀

---
+++ 01-color-switcher.b1ef7b6f.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),a=null;t.addEventListener("click",(function(n){a=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3),t.disabled=!0,e.disabled=!1})),e.addEventListener("click",(function(n){clearInterval(a),t.disabled=!1,e.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.b1ef7b6f.js.map
